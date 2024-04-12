49fbf22710807cbf6cf8581634777c426c553d7b
tomkcey <tomkcey@gmail.com>
2024-04-11 20:34:36 -0400

    refactor: move cli actions into index to make the cli integration neutral

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { otlpSdk } from "./utils/otlp";
 import { logger } from "./utils/logging";
 import { MaybePromise } from "./utils/async";
 import { Cli } from "./integrations/cli/cli";
+import { main as greeting } from "./functions/greeting";
+import { KoaHttp } from "./integrations/http";
+import { config } from "./utils/config";
 
 async function main<T>(execute: () => MaybePromise<T>) {
 	process.on("SIGINT", () => {
@@ -15,4 +18,46 @@ async function main<T>(execute: () => MaybePromise<T>) {
 	await execute();
 }
 
-void main(Cli.run);
+void main(() =>
+	Cli.run(
+		(command) =>
+			command
+				.name(Cli.CommandName.Serve)
+				.description("Start the server")
+				.option("-p, --port <port>", "Port to listen on")
+				.option("--provider <provider>", "Provider to use (Koa)")
+				.action(async (opts) => {
+					logger.info("Received command to start server.");
+
+					const port = opts.port ?? config.port;
+					process.env.PORT = port.toString();
+
+					if (opts.provider === Cli.ServeProvider.Koa) {
+						logger.info(
+							`Starting http server using provider ${opts.provider}`,
+						);
+
+						const http = KoaHttp.getKoaHttpServer();
+						return http.start(port);
+					}
+
+					throw new Error(`Unsupported provider ${opts.provider}`);
+				}),
+		(command) =>
+			command
+				.name(Cli.CommandName.Function)
+				.description("Execute a function")
+				.option("-n, --name <name>", "Function to execute")
+				.action(async (opts) => {
+					logger.info("Received command to execute function");
+
+					if (opts.name === Cli.FunctionName.Greeting) {
+						logger.info(`Executing function ${opts.name}`);
+
+						return greeting();
+					}
+
+					throw new Error(`Unsupported function ${opts.name}`);
+				}),
+	),
+);
--- src/integrations/cli/cli.ts
+++ src/integrations/cli/cli.ts
@@ -1,26 +1,10 @@
 import { Command, program } from "commander";
-import { config } from "../../utils/config";
-import { KoaHttp } from "../http";
-import { main } from "../../functions/greeting";
-
-enum CommandName {
-	Serve = "serve",
-	Function = "function",
-}
-
-enum ServeProvider {
-	Koa = "koa",
-}
-
-enum FunctionName {
-	Greeting = "greeting",
-}
 
 export class Cli {
-	protected p: Command = program;
+	constructor(protected p: Command = program) {}
 
-	public command(command: Command) {
-		this.p.addCommand(command);
+	public add(factory: (command: Command) => Command) {
+		this.p.addCommand(factory(new Command()));
 		return this;
 	}
 
@@ -30,56 +14,30 @@ export class Cli {
 }
 
 export namespace Cli {
+	export enum CommandName {
+		Serve = "serve",
+		Function = "function",
+	}
+
+	export enum ServeProvider {
+		Koa = "koa",
+	}
+
+	export enum FunctionName {
+		Greeting = "greeting",
+	}
+
 	let cli: Cli | null = null;
 
 	export function getCli() {
 		if (!cli) {
-			cli = new Cli()
-				.command(
-					new Command(CommandName.Serve)
-						.description("Start the server")
-						.option("-p, --port <port>", "Port to listen on")
-						.option(
-							"--provider <provider>",
-							"Provider to use (Koa)",
-						)
-						.action(async (opts) => {
-							const port = opts.port ?? config.port;
-							process.env.PORT = port.toString();
-
-							switch (opts.provider) {
-								case ServeProvider.Koa: {
-									const http = KoaHttp.getKoaHttpServer();
-									return http.start(port);
-								}
-								default:
-									throw new Error(
-										`Unsupported provider ${opts.provider}`,
-									);
-							}
-						}),
-				)
-				.command(
-					new Command(CommandName.Function)
-						.description("Execute a function")
-						.option("-n, --name <name>", "Function to execute")
-						.action(async (opts) => {
-							switch (opts.name) {
-								case FunctionName.Greeting:
-									return main();
-								default:
-									throw new Error(
-										`Unsupported function ${opts.name}`,
-									);
-							}
-						}),
-				);
+			cli = new Cli();
 		}
 		return cli;
 	}
 
-	export async function run() {
+	export async function run(...factories: ((command: Command) => Command)[]) {
 		const cli = getCli();
-		return cli.parse();
+		return factories.reduce((acc, cur) => acc.add(cur), cli).parse();
 	}
 }
