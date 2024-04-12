250481e974db98471f5cece50bfafb6b83ab065a
Thomas Caissie <30733005+tomkcey@users.noreply.github.com>
2024-04-11 20:55:31 -0400

    refactor: remove excess cruft in pg test

--- src/integrations/database/pg.test.ts
+++ src/integrations/database/pg.test.ts
@@ -7,267 +7,143 @@ import {
 } from "../../utils/errors";
 import { QueryResultRow } from "pg";
 import { Database } from "./db";
-import { randomUUID } from "crypto";
 import { sequential } from "../../utils/async";
 
-type Achievement = {
-	id: string;
+type Resource = {
+	id: number;
 	name: string;
-	description: string;
 	createdAt: Date;
 	updatedAt: Date;
 };
 
-class AchievementDatabase extends PostgresDatabase {
+class ResourceDatabase extends PostgresDatabase {
 	constructor(executor?: PostgresExecutor) {
 		super(executor);
 	}
 
-	private mapRowToAchievement(row: QueryResultRow): Achievement {
+	private mapRowToResource(row: QueryResultRow): Resource {
 		return {
 			id: row.id,
 			name: row.name,
-			description: row.description,
 			createdAt: row.created_at,
 			updatedAt: row.updated_at,
 		};
 	}
 
-	public async listAchievements(paginate: Paginate): Promise<Achievement[]> {
+	public async listResources(paginate: Paginate): Promise<Resource[]> {
 		const rows = await this.list(
-			SQL`SELECT * FROM achievements LIMIT ${paginate.take} OFFSET ${paginate.skip};`,
+			SQL`SELECT * FROM resources LIMIT ${paginate.take} OFFSET ${paginate.skip};`,
 		);
 
-		return rows.map((row) => this.mapRowToAchievement(row));
+		return rows.map((row) => this.mapRowToResource(row));
 	}
 
-	public async getAchievementById(id: string): Promise<Achievement> {
+	public async getResourceById(id: number): Promise<Resource> {
 		const row = await this.unique(
-			SQL`SELECT * FROM achievements WHERE id = ${id};`,
+			SQL`SELECT * FROM resources WHERE id = ${id};`,
 		);
 
 		if (!row) {
-			throw new NotFoundError(`Achievement with id ${id} not found`);
+			throw new NotFoundError(`Resource with id ${id} not found`);
 		}
 
-		return this.mapRowToAchievement(row);
+		return this.mapRowToResource(row);
 	}
 
 	private async insert(
-		data: Omit<Achievement, "id" | "createdAt" | "updatedAt">,
-	): Promise<Achievement> {
+		data: Omit<Resource, "id" | "createdAt" | "updatedAt">,
+	): Promise<Resource> {
 		const row = await this.unique(
-			SQL`INSERT INTO achievements (name, description) VALUES (${data.name}, ${data.description}) RETURNING *;`,
+			SQL`INSERT INTO resources (name) VALUES (${data.name}) RETURNING *;`,
 		);
 
 		if (!row) {
-			throw new InternalServerError("Failed to create achievement");
+			throw new InternalServerError("Failed to create resource");
 		}
 
-		return this.mapRowToAchievement(row);
+		return this.mapRowToResource(row);
 	}
 
-	public async createAchievement(
-		data: Omit<Achievement, "id" | "createdAt" | "updatedAt">,
-	): Promise<Achievement> {
+	public async createResource(
+		data: Omit<Resource, "id" | "createdAt" | "updatedAt">,
+	): Promise<Resource> {
 		return this.transaction(async (executor) => {
-			const db = new AchievementDatabase(executor);
+			const db = new ResourceDatabase(executor);
 
-			const achievement = await db.insert(data);
+			const resource = await db.insert(data);
 
-			const auditDb = new AuditDatabase(executor);
-
-			await auditDb.createAudit({
-				previousData: null,
-				currentData: JSON.stringify(achievement),
-				event: AuditEvent.Created,
-				version: 1,
-				refTable: "achievements",
-				refId: achievement.id,
-			});
-
-			return achievement;
+			return resource;
 		});
 	}
 
 	private async update(
-		id: string,
-		data: Partial<Omit<Achievement, "id" | "createdAt">>,
-	): Promise<Achievement> {
+		id: number,
+		data: Partial<Omit<Resource, "id" | "createdAt">>,
+	): Promise<Resource> {
 		if (Object.keys(data).length === 0) {
 			throw new BadRequestError("No data provided to update");
 		}
 
 		const nameSetStatement = data.name ? `name = '${data.name}'` : null;
-		const descriptionSetStatement = data.description
-			? `description = '${data.description}'`
-			: null;
-		const updatedAtSetStatement = `updated_at = now()`;
+		const updatedAt = `updated_at = now()`;
 
-		const setStatement = [
-			nameSetStatement,
-			descriptionSetStatement,
-			updatedAtSetStatement,
-		]
+		const setStatement = [nameSetStatement, updatedAt]
 			.filter((statement) => statement !== null)
 			.join();
 
 		const row = await this.unique(
-			SQL`UPDATE achievements SET `
+			SQL`UPDATE resources SET `
 				.append(setStatement)
 				.append(SQL` WHERE id = ${id} RETURNING *;`),
 		);
 
 		if (!row) {
-			throw new InternalServerError("Failed to update achievement");
+			throw new InternalServerError("Failed to update resource");
 		}
 
-		return this.mapRowToAchievement(row);
+		return this.mapRowToResource(row);
 	}
 
-	public async updateAchievement(
-		id: string,
-		data: Partial<Omit<Achievement, "id" | "createdAt">>,
-	): Promise<Achievement> {
+	public async updateResource(
+		id: number,
+		data: Partial<Omit<Resource, "id" | "createdAt">>,
+	): Promise<Resource> {
 		return this.transaction(async (executor) => {
-			const db = new AchievementDatabase(executor);
+			const db = new ResourceDatabase(executor);
 
-			const previousAchievement = await db.getAchievementById(id);
-			const updatedAchievement = await db.update(id, data);
+			const updatedResource = await db.update(id, data);
 
-			const auditDb = new AuditDatabase(executor);
-
-			await auditDb.createAudit({
-				previousData: JSON.stringify(previousAchievement),
-				currentData: JSON.stringify(updatedAchievement),
-				event: AuditEvent.Updated,
-				version: 1,
-				refTable: "achievements",
-				refId: updatedAchievement.id,
-			});
-
-			return updatedAchievement;
+			return updatedResource;
 		});
 	}
 
-	private async delete(id: string) {
-		await this.execute(SQL`DELETE FROM achievements WHERE id = ${id};`);
-	}
+	public async deleteResource(id: number): Promise<void> {
+		const resource = await this.getResourceById(id);
 
-	public async deleteAchievement(id: string) {
-		return this.transaction(async (executor) => {
-			const db = new AchievementDatabase(executor);
+		if (!resource) {
+			throw new NotFoundError(`Resource with id ${id} not found`);
+		}
 
-			const achievement = await db.getAchievementById(id);
-			await db.delete(id);
+		await this.transaction(async (executor) => {
+			const db = new ResourceDatabase(executor);
 
-			const auditDb = new AuditDatabase(executor);
-
-			await auditDb.createAudit({
-				previousData: JSON.stringify(achievement),
-				currentData: null,
-				event: AuditEvent.Deleted,
-				version: 1,
-				refTable: "achievements",
-				refId: achievement.id,
-			});
+			await db.execute(SQL`DELETE FROM resources WHERE id = ${id};`);
 		});
-	}
-}
-
-enum AuditEvent {
-	Created = "created",
-	Updated = "updated",
-	Deleted = "deleted",
-}
-
-type Audit = {
-	id: string;
-	version: number;
-	event: AuditEvent;
-	refTable: string;
-	refId: string;
-	previousData: string | null;
-	currentData: string | null;
-	createdAt: Date;
-};
-
-class AuditDatabase extends PostgresDatabase {
-	constructor(executor?: PostgresExecutor) {
-		super(executor);
-	}
-
-	private mapRowToAudit(row: QueryResultRow): Audit {
-		return {
-			id: row.id,
-			version: row.version,
-			event: row.event,
-			refTable: row.ref_table,
-			refId: row.ref_id,
-			previousData: row.previous_data,
-			currentData: row.current_data,
-			createdAt: row.created_at,
-		};
-	}
-
-	public async listAudits(paginate: Paginate): Promise<Audit[]> {
-		const rows = await this.list(
-			SQL`SELECT * FROM audits LIMIT ${paginate.take} OFFSET ${paginate.skip};`,
-		);
-
-		return rows.map((row) => this.mapRowToAudit(row));
-	}
-
-	public async getAuditById(id: string): Promise<Audit> {
-		const row = await this.unique(
-			SQL`SELECT * FROM audits WHERE id = ${id};`,
-		);
-
-		if (!row) {
-			throw new NotFoundError(`Audit with id ${id} not found`);
-		}
-
-		return this.mapRowToAudit(row);
-	}
-
-	public async createAudit(data: Omit<Audit, "id" | "createdAt">) {
-		const row = await this.unique(
-			SQL`INSERT INTO audits (version, event, ref_table, ref_id, previous_data, current_data) VALUES (${data.version}, ${data.event}, ${data.refTable}, ${data.refId}, ${data.previousData}, ${data.currentData}) RETURNING *;`,
-		);
-
-		if (!row) {
-			throw new InternalServerError("Failed to create audit");
-		}
-
-		return this.mapRowToAudit(row);
 	}
 }
 
 class SeedDatabase extends PostgresDatabase {
 	public async setup() {
-		await this.execute(SQL`CREATE TABLE IF NOT EXISTS achievements (
-            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-            name TEXT NOT NULL,
-            description TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-            );`);
-
-		await this.execute(SQL`CREATE TABLE IF NOT EXISTS audits (
-                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                version INT NOT NULL,
-                event TEXT NOT NULL,
-                ref_table TEXT NOT NULL,
-                ref_id UUID NOT NULL,
-                previous_data TEXT,
-                current_data TEXT,
-                created_at TIMESTAMP DEFAULT NOW()
-                );`);
+		await this.execute(SQL`CREATE TABLE IF NOT EXISTS resources (
+		 	id serial primary key,
+		 	name text not null,
+		 	created_at timestamptz not null default now(),
+		 	updated_at timestamptz not null default now()
+		)`);
 	}
 
 	public async teardown() {
-		await this.execute(SQL`DROP TABLE IF EXISTS achievements;`);
-		await this.execute(SQL`DROP TABLE IF EXISTS audits;`);
+		await this.execute(SQL`DROP TABLE IF EXISTS resources;`);
 	}
 }
 
@@ -286,163 +162,119 @@ describe(Database.name, () => {
 		await seedDb.shutdown();
 	});
 
-	describe(AchievementDatabase.prototype.createAchievement.name, () => {
-		it("creates and returns the achievement, also creating an audit entity", async () => {
-			const spy = jest.spyOn(AuditDatabase.prototype, "createAudit");
-
-			const db = new AchievementDatabase();
-			const achievement = await db.createAchievement({
+	describe(ResourceDatabase.prototype.createResource.name, () => {
+		it("creates and returns the resource", async () => {
+			const db = new ResourceDatabase();
+			const resource = await db.createResource({
 				name: "test",
-				description: "test",
 			});
 
-			expect(achievement).toEqual({
-				id: expect.any(String),
+			expect(resource).toEqual({
+				id: 1,
 				name: "test",
-				description: "test",
 				createdAt: expect.any(Date),
 				updatedAt: expect.any(Date),
 			});
-			expect(spy).toHaveBeenCalledWith({
-				version: 1,
-				event: "created",
-				refTable: "achievements",
-				refId: achievement.id,
-				previousData: null,
-				currentData: JSON.stringify(achievement),
-			});
 		});
 	});
 
-	describe(AchievementDatabase.prototype.getAchievementById.name, () => {
-		it("returns the achievement associated with the given id", async () => {
-			const db = new AchievementDatabase();
-			const achievement = await db.createAchievement({
+	describe(ResourceDatabase.prototype.getResourceById.name, () => {
+		it("returns the resource associated with the given id", async () => {
+			const db = new ResourceDatabase();
+			const resource = await db.createResource({
 				name: "test",
-				description: "test",
 			});
 
-			const result = await db.getAchievementById(achievement.id);
+			const result = await db.getResourceById(resource.id);
 
-			expect(result).toEqual(achievement);
+			expect(result).toEqual(resource);
 		});
 
-		it("throws a NotFoundError if the achievement does not exist", async () => {
-			const db = new AchievementDatabase();
+		it("throws a NotFoundError if the resource does not exist", async () => {
+			const db = new ResourceDatabase();
 
-			await expect(db.getAchievementById(randomUUID())).rejects.toThrow(
-				NotFoundError,
-			);
+			await expect(db.getResourceById(9)).rejects.toThrow(NotFoundError);
 		});
 	});
 
-	describe(AchievementDatabase.prototype.listAchievements.name, () => {
-		it("returns a list of achievements", async () => {
-			const db = new AchievementDatabase();
-			const achievements = await sequential(
+	describe(ResourceDatabase.prototype.listResources.name, () => {
+		it("returns a list of resources", async () => {
+			const db = new ResourceDatabase();
+			const resources = await sequential(
 				Array.from({ length: 5 }),
-				async () =>
-					db.createAchievement({ name: "test", description: "test" }),
+				async () => db.createResource({ name: "test" }),
 			);
 
-			const result = await db.listAchievements({ skip: 0, take: 5 });
+			const result = await db.listResources({ skip: 0, take: 5 });
 
-			expect(result).toEqual(achievements);
+			expect(result).toEqual(resources);
 		});
 
-		it("returns a paginated list of achievements", async () => {
-			const db = new AchievementDatabase();
-			const achievements = await sequential(
+		it("returns a paginated list of resources", async () => {
+			const db = new ResourceDatabase();
+			const resources = await sequential(
 				Array.from({ length: 5 }),
-				async () =>
-					db.createAchievement({ name: "test", description: "test" }),
+				async () => db.createResource({ name: "test" }),
 			);
 
-			const result = await db.listAchievements({ skip: 2, take: 2 });
+			const result = await db.listResources({ skip: 2, take: 2 });
 
-			expect(result).toEqual(achievements.slice(2, 4));
+			expect(result).toEqual(resources.slice(2, 4));
 		});
 	});
 
-	describe(AchievementDatabase.prototype.updateAchievement.name, () => {
-		it("updates and returns the achievement, also creating an audit entity", async () => {
-			const spy = jest.spyOn(AuditDatabase.prototype, "createAudit");
-
-			const db = new AchievementDatabase();
-			const achievement = await db.createAchievement({
+	describe(ResourceDatabase.prototype.updateResource.name, () => {
+		it("updates and returns the resource", async () => {
+			const db = new ResourceDatabase();
+			const resource = await db.createResource({
 				name: "test",
-				description: "test",
 			});
 
-			const updatedAchievement = await db.updateAchievement(
-				achievement.id,
-				{ name: "anotherTest" },
-			);
-
-			expect(updatedAchievement).toEqual({
-				id: expect.any(String),
+			const updatedResource = await db.updateResource(resource.id, {
 				name: "anotherTest",
-				description: "test",
+			});
+
+			expect(updatedResource).toEqual({
+				id: resource.id,
+				name: "anotherTest",
 				createdAt: expect.any(Date),
 				updatedAt: expect.any(Date),
 			});
-			expect(updatedAchievement.updatedAt).not.toEqual(
-				updatedAchievement.createdAt,
+			expect(updatedResource.updatedAt).not.toEqual(
+				updatedResource.createdAt,
 			);
-			expect(spy).toHaveBeenCalledWith({
-				version: 1,
-				event: "updated",
-				refTable: "achievements",
-				refId: updatedAchievement.id,
-				previousData: JSON.stringify(achievement),
-				currentData: JSON.stringify(updatedAchievement),
-			});
 		});
 
 		it("throws a BadRequestError if no data is provided", async () => {
-			const db = new AchievementDatabase();
-			const achievement = await db.createAchievement({
+			const db = new ResourceDatabase();
+			const resource = await db.createResource({
 				name: "test",
-				description: "test",
 			});
 
-			await expect(
-				db.updateAchievement(achievement.id, {}),
-			).rejects.toThrow(BadRequestError);
+			await expect(db.updateResource(resource.id, {})).rejects.toThrow(
+				BadRequestError,
+			);
 		});
 	});
 
-	describe(AchievementDatabase.prototype.deleteAchievement.name, () => {
-		it("deletes the achievement and creates an audit entity", async () => {
-			const spy = jest.spyOn(AuditDatabase.prototype, "createAudit");
-
-			const db = new AchievementDatabase();
-			const achievement = await db.createAchievement({
+	describe(ResourceDatabase.prototype.deleteResource.name, () => {
+		it("deletes the resource", async () => {
+			const db = new ResourceDatabase();
+			const resource = await db.createResource({
 				name: "test",
-				description: "test",
 			});
 
-			await db.deleteAchievement(achievement.id);
+			await db.deleteResource(resource.id);
 
-			await expect(db.getAchievementById(achievement.id)).rejects.toThrow(
+			await expect(db.getResourceById(resource.id)).rejects.toThrow(
 				NotFoundError,
 			);
-			expect(spy).toHaveBeenCalledWith({
-				version: 1,
-				event: "deleted",
-				refTable: "achievements",
-				refId: achievement.id,
-				previousData: JSON.stringify(achievement),
-				currentData: null,
-			});
 		});
 
-		it("throws a NotFoundError if the achievement does not exist", async () => {
-			const db = new AchievementDatabase();
+		it("throws a NotFoundError if the resource does not exist", async () => {
+			const db = new ResourceDatabase();
 
-			await expect(db.deleteAchievement(randomUUID())).rejects.toThrow(
-				NotFoundError,
-			);
+			await expect(db.deleteResource(9)).rejects.toThrow(NotFoundError);
 		});
 	});
 });
